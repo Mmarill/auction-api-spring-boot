e165699f34d194bf0275271d04df9470d104082f
AiDroopy <AiDroopy@users.noreply.github.com>
2022-04-19 14:17:02 +0200

    Updated BidsController

--- auction-api-spring/src/main/java/com/example/auctionapispring/bids/BidsController.java
+++ auction-api-spring/src/main/java/com/example/auctionapispring/bids/BidsController.java
@@ -1,4 +1,13 @@
 package com.example.auctionapispring.bids;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/bid")
 public class BidsController {
+
+    @Autowired
+    BidsService bidsService;
 }
