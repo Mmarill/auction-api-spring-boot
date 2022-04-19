504c61e2d0f9529035300315359551df920c3f62
AiDroopy <AiDroopy@users.noreply.github.com>
2022-04-19 14:21:24 +0200

    Updated BidsRepository with findById

--- auction-api-spring/src/main/java/com/example/auctionapispring/bids/BidsRepository.java
+++ auction-api-spring/src/main/java/com/example/auctionapispring/bids/BidsRepository.java
@@ -2,5 +2,8 @@ package com.example.auctionapispring.bids;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface BidsRepository extends MongoRepository {
+    Optional<Bid> findById(String id);
 }
