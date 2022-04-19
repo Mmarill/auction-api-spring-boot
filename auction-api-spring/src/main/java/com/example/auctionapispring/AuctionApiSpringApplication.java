c139cf0383dd8a61e148b1025c6ef3bd49c97649
Mmarill <Max.henkesson@gmail.com>
2022-04-19 10:42:20 +0200

    Initial commit

---
+++ auction-api-spring/src/main/java/com/example/auctionapispring/AuctionApiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.auctionapispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionApiSpringApplication.class, args);
+	}
+
+}
---
+++ auction-api-spring/src/test/java/com/example/auctionapispring/AuctionApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auctionapispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
