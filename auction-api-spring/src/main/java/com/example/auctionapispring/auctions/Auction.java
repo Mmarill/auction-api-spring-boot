b1582475f7b506624ad7a428f913926770f16d2a
AiDroopy <AiDroopy@users.noreply.github.com>
2022-04-20 08:53:24 +0200

    Updated BidService to prevent HALT!

--- auction-api-spring/src/main/java/com/example/auctionapispring/auctions/Auction.java
+++ auction-api-spring/src/main/java/com/example/auctionapispring/auctions/Auction.java
@@ -1,5 +1,6 @@
 package com.example.auctionapispring.auctions;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalTime;
--- auction-api-spring/src/main/java/com/example/auctionapispring/bids/BidsRepository.java
+++ auction-api-spring/src/main/java/com/example/auctionapispring/bids/BidsRepository.java
@@ -1,10 +1,13 @@
 package com.example.auctionapispring.bids;
 
+import com.example.auctionapispring.auctions.Auction;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
-public interface BidsRepository extends MongoRepository {
-    Optional<Bid> findById(String id);
+public interface BidsRepository extends MongoRepository <Bid, String>{
+    //Optional<Bid> findById(String id);
+
 }
 
