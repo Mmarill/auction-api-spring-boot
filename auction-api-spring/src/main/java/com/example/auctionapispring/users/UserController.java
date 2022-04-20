d6c062cf57bd6247c924083208ef76f06004a82c
pirtyraven <89965279+pirtyraven@users.noreply.github.com>
2022-04-20 10:46:53 +0200

    Create UserController.java
    Added UserController to fetch all users

---
+++ auction-api-spring/src/main/java/com/example/auctionapispring/users/UserController.java
@@ -0,0 +1,22 @@
+package com.example.auctionapispring.users;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/all")
+    public List <User> getUsers() {
+        return userRepository.findAll();
+    }
+
+}
