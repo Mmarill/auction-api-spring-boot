1c0f0eff41498c5837860f6c63f11a805d951df2
Mmarill <Max.henkesson@gmail.com>
2022-04-20 20:23:27 +0200

    Har fyllat på auction classerna

--- auction-api-spring/src/main/java/com/example/auctionapispring/users/UserController.java
+++ auction-api-spring/src/main/java/com/example/auctionapispring/users/UserController.java
@@ -1,9 +1,9 @@
 package com.example.auctionapispring.users;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,4 +19,16 @@ public class UserController {
         return userRepository.findAll();
     }
 
+    @PutMapping("/update/{id}")
+    public ResponseEntity<User> updateUser(
+            @PathVariable String id,
+            @RequestBody User userDetails) throws ResourceNotFoundException {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found on : "+ id));
+
+        user.setUsername(userDetails.getUsername());
+        user.setPassword(userDetails.getPassword());
+        final User updatedUser = userRepository.save(user);
+        return ResponseEntity.ok(updatedUser);
+    }
 }
