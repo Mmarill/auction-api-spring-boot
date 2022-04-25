9570a85d4dcc07d303acfc83d0312e07bba9ae40
Biirdmaan <deki_s@hotmail.com>
2022-04-25 19:32:06 +0200

    test

--- auction-api-spring/src/main/java/com/example/auctionapispring/files/FileController.java
+++ auction-api-spring/src/main/java/com/example/auctionapispring/files/FileController.java
@@ -5,6 +5,7 @@ import com.example.auctionapispring.payload.response.MessageResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,7 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 import java.util.Objects;
 
-@RestController
+@Controller
 @RequestMapping("/api/images")
 public class FileController {
 
