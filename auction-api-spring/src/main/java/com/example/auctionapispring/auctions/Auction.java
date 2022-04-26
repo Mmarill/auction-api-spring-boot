1bcf30fe6f25655dc4c6067b27ae0e14fc16c699
Mmarill <Max.henkesson@gmail.com>
2022-04-26 14:26:46 +0200

    Update Auction.java

--- auction-api-spring/src/main/java/com/example/auctionapispring/auctions/Auction.java
+++ auction-api-spring/src/main/java/com/example/auctionapispring/auctions/Auction.java
@@ -1,5 +1,6 @@
 package com.example.auctionapispring.auctions;
 
+import com.example.auctionapispring.bids.Bid;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
